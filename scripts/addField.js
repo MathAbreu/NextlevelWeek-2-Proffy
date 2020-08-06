2752c1f284407fa3db92a1656858ad96750bf6b5
Matheus Barreto <mathbarreto2202@gmail.com>
2020-08-05 22:41:42 -0300

    Finalização do Front-end com introdução ao JS do app.

---
+++ scripts/addField.js
@@ -0,0 +1,28 @@
+//Procurar o botão
+//Quando clicar no botão
+//Executar uma ação
+
+    //Duplicar os campos
+
+    //Colocar na página
+//
+
+document.querySelector("#add-times")
+.addEventListener('click', cloneField)
+
+function cloneField(){
+    //Que campo vou duplicar ?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true) // Boolean
+    
+    //Limpar que campos ?
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    //Para cada campo, limpar
+    fields.forEach(function(field){
+        //Pegar o Field atual e limpe o campo
+        field.value = ""
+    })
+
+    //Colocar onde na página ?
+    document.querySelector('#schedule-items').appendChild(fields)
+}
